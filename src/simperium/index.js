39dd4cbc45fbaf9327ef80224ca00112db0a75df
Beau Collins <beaucollins@gmail.com>
2015-12-04 00:09:07 -0800

    set NODE_PATH for nicer imports

--- src/simperium/index.js
+++ src/simperium/index.js
@@ -1,10 +1,12 @@
 import User from './user'
 import Client from './client'
 import Auth from './auth'
+import Bucket from './bucket'
+import Channel from './channel'
 import util from './util'
 
 export default function( appId, token, options ) {
 	return new Client( appId, token, options );
 }
 
-export { Auth, User, Client, util }
+export { Auth, User, Client, util, Bucket, Channel }
